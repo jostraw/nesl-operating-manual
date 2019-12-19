82fc12eb5af5f67b1ae3ac96f3edf75e4a1bd42e
Harry Trimble <harryroberttrimble@gmail.com>
2019-12-19 14:21:28 +0000

    Add script to disciplay today's date
    Add script to disciplay today's date to make it easier for people who

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,3 +8,18 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+// Current date for when people print web page
+$(document).ready(function(){
+
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+  today = dd + ' ' + months[mm-1] + ' ' + yyyy;
+  console.log(today);
+  $('#currentdate').html(today);
+});
