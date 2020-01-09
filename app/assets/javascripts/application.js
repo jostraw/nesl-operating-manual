8904b65242220e3f22e6f1ae02bfead398566702
Harry Trimble <harryroberttrimble@gmail.com>
2020-01-09 13:34:08 +0000

    Unbug bug fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,7 +27,10 @@ $(document).ready(function(){
 // 'Copy template' button script
 function copyFunction() {
   const copyText = document.getElementById("myInput").textContent;
+  const textArea = document.createElement('textarea');
   textArea.textContent = copyText;
+  document.body.append(textArea);
+  textArea.select();
   document.execCommand("copy");
 }
 
